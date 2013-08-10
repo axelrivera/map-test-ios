3e075409c8d7e7c245092c8fe003c37ae7872722
Axel Rivera <axel@riveralabs.com>
2013-08-10 13:58:32 -0400

    Initial Commit

---
+++ MapTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapTest
+//
+//  Created by Axel Rivera on 8/10/13.
+//  Copyright (c) 2013 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
